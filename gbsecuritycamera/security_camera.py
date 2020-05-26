724583e6339cc9558f683f5098c47e5ebda802fd
idoheinemann <idohaineman@gmail.com>
2020-05-26 18:16:37 +0300

    added docs and what not

--- gbsecuritycamera/security_camera.py
+++ gbsecuritycamera/security_camera.py
@@ -1,7 +1,7 @@
 from base64 import standard_b64encode
 import time
 from threading import Thread
-from typing import Tuple
+from typing import Tuple, List
 
 import gbvision as gbv
 import requests
@@ -10,6 +10,15 @@ import numpy as np
 
 class SecurityCamera(gbv.USBCamera):
     def __init__(self, ip: str, rtsp_port=554, data=gbv.UNKNOWN_CAMERA, user='admin', password=''):
+        """
+        A class connecting to the security camera
+
+        :param ip: the ip of the camera
+        :param rtsp_port: the port to which the camera streams using rstp (default is 554)
+        :param data: the camera data object of the camera
+        :param user: the user name of the camera (default is admin)
+        :param password: the password to the username (default is empty)
+        """
         self.ip = ip
         self.rtsp_port = rtsp_port
         self.user = user
@@ -38,16 +47,35 @@ class SecurityCamera(gbv.USBCamera):
         headers = {'Authorization': f'Basic {auth.decode()}'}
         return requests.get(f'http://{self.ip}/{action}', headers=headers, params=params)
 
-    def __send_action(self, params):
+    def __request_with_random(self, action, params):
+        return self.__request(action, params + (('random', np.random.rand()),))
+
+    def __send_ptz(self, params):
         self.__request('ptz.cgi', params)
 
     def set_power(self, speed_x, speed_y):
-        self.__send_action(self.__create_continues_move_request(speed_x, speed_y))
+        """
+        sets the power to the camera's engines
+
+        :param speed_x: the speed by which to move the x axis (sideways)
+        :param speed_y: the speed by which to move the y axis (up/down)
+        """
+        self.__send_ptz(self.__create_continues_move_request(speed_x, speed_y))
 
     def stop(self):
+        """
+        stops the camera's movement
+        """
         self.set_power(0, 0)
 
     def set_power_timeout(self, speed_x, speed_y, timeout):
+        """
+        moves the camera in a constant speed for timeout seconds (unblocking)
+
+        :param speed_x: the speed in the x axis by which to move
+        :param speed_y: the speed on the y axis by which to move
+        :param timeout: the time (in seconds) to move the camera
+        """
         def __proc():
             self.set_power(speed_x, speed_y)
             time.sleep(timeout)
@@ -56,4 +84,37 @@ class SecurityCamera(gbv.USBCamera):
         Thread(target=__proc).start()
 
     def to_home(self, speed=50):
-        self.__send_action(self.__create_move_request('home', speed))
+        """
+        moves the camera to the starting position (home state)
+
+        :param speed: the speed by which to move (default 50)
+        """
+        self.to_state('home', speed)
+
+    def get_states_list(self) -> List[str]:
+        """
+        gets the full list of saved state (without 'home')
+
+        :return: a list of all saved state, ordered by the time they were created (excluding 'home')
+        """
+        answer = self.__request_with_random('asp/config.cgi', (
+            ('action', 'list'), ('group', 'PTZ.PresetPos,GuardTour,Fisheye'))).content
+        return [x.split('=')[1].split('~')[0] for x in answer.decode('ascii').strip('\n\r').split('\n')]
+
+    def save_state(self, state_name: str):
+        """
+        saves the current camera location as a state that can be returned to at any time (see \
+        `gbsecuritycamera.SecurityCamera.to_state`)
+
+        :param state_name: the name of the state, if this state already exists it will override it
+        """
+        self.__request_with_random('configptz.cgi', (('setserverpresetname', state_name),))
+
+    def to_state(self, state, speed=50):
+        """
+        moves the camera to a saved state
+
+        :param state: the name of the saved state (see `gbsecuritycamera.SecurityCamera.save_state`)
+        :param speed: the speed in which to move (default 50)
+        """
+        self.__send_ptz(self.__create_move_request(state, speed))
